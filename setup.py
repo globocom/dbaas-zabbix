ba72f2a24b5dd3918004bd8f609dadb8beacac61
Ricardo Dias <ricardo@ricardodias.org>
2014-11-26 16:11:53 -0200

    Change version to 0.1.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_zabbix',
-    version='0.1.3',
+    version='0.1.4',
     description='A Zabbix Integration for DBaaS.',
     long_description=readme + '\n\n' + history,
     author='Felippe Raposo',
