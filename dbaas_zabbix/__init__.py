76b05be5540129050ddb4ecddb1c785e0aeb2793
felippemr <raposo.felippe@gmail.com>
2015-08-13 14:57:44 -0300

    Add factory_for method

--- dbaas_zabbix/__init__.py
+++ dbaas_zabbix/__init__.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 from dbaas_zabbix.dbaas_api import DatabaseAsAServiceApi
 from dbaas_zabbix.provider_factory import ProviderFactory
+from pyzabbix import ZabbixAPI
 
 
 def factory_for(**kwargs):
     databaseinfra = kwargs['databaseinfra']
+    credentials = kwargs['credentials']
     del kwargs['databaseinfra']
+    del kwargs['credentials']
 
-    dbaas_api = DatabaseAsAServiceApi(databaseinfra)
+    dbaas_api = DatabaseAsAServiceApi(databaseinfra, credentials)
 
-    return ProviderFactory(dbaas_api, **kwargs)
+    return ProviderFactory(dbaas_api, zabbix_api=ZabbixAPI, **kwargs)
