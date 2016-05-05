8ea39dd66d15fdeabfa2fac87e67b901efa8e63d
felippemr <raposo.felippe@gmail.com>
2016-05-05 18:47:58 -0300

    Add new providers classes for mongodb3

--- dbaas_zabbix/database_providers.py
+++ dbaas_zabbix/database_providers.py
@@ -215,6 +215,33 @@ class RedisHighAvailabilityZabbixProvider(RedisZabbixProvider):
     __version__ = '2.8.17'
 
 
+class MongoDBThreeDotZeroSingleZabbixProvider(DatabaseZabbixProvider):
+    __provider_name__ = 'mongodb'
+    __is_ha__ = False
+    __version__ = '3.0.8'
+
+    def create_database_monitors(self):
+        clientgroup = self.extra_clientgroup
+        for instance in self.instances:
+            self._create_mongo_three_monitors(
+                host=instance.dns, alarm="group", clientgroup=clientgroup,
+                **self.get_database_monitors_extra_parameters())
+
+
+class MongoDBThreeDotZeroHighAvailabilityZabbixProvider(DatabaseZabbixProvider):
+    __provider_name__ = 'mongodb'
+    __is_ha__ = True
+    __version__ = '3.0.8'
+
+    def create_database_monitors(self,):
+        clientgroup = self.extra_clientgroup
+        for instance in self.instances:
+            self._create_mongo_three_monitors(
+                host=instance.dns, alarm="group",
+                clientgroup=clientgroup, replicaset="1",
+                **self.get_database_monitors_extra_parameters())
+
+
 class FakeSingleZabbixProvider(DatabaseZabbixProvider):
     __provider_name__ = 'fake'
     __is_ha__ = False
