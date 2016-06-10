d9bb7039ee959fbe8bfc39f8cc41a66e573e03d2
felippemr <raposo.felippe@gmail.com>
2016-06-10 18:24:36 -0300

    Change zabbix version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_zabbix',
-    version='0.4.1',
+    version='0.4.2',
     description='A Zabbix Integration for DBaaS.',
     long_description=readme + '\n\n' + history,
     author='Felippe Raposo',
