84f6abef828381198cad4a5dc3a4c257b942ddcf
Ricardo Dias <ricardo@ricardodias.org>
2018-12-12 18:12:25 -0200

    new app version 0.6.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='dbaas_zabbix',
-    version='0.6.0',
+    version='0.6.1',
     description='A Zabbix Integration for DBaaS.',
     long_description=readme + '\n\n' + history,
     author='Felippe Raposo',
