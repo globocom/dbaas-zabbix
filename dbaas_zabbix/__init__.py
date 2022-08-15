df796462b07f7470d86f4d0622a414c956a45ced
Erkmann <mateus.erkmann_esx@prestador.globo>
2022-08-15 10:16:45 -0300

    logs pyzabbix com variavel de ambiente

--- dbaas_zabbix/__init__.py
+++ dbaas_zabbix/__init__.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import logging
+import os
 import sys
 
 from dbaas_zabbix.dbaas_api import DatabaseAsAServiceApi
 from dbaas_zabbix.provider_factory import ProviderFactory
 from pyzabbix import ZabbixAPI
 
-stream = logging.StreamHandler(sys.stdout)
-stream.setLevel(logging.DEBUG)
-log = logging.getLogger('pyzabbix')
-log.addHandler(stream)
-log.setLevel(logging.DEBUG)
+LOG = logging.getLogger(__name__)
+
+
+def set_integration_logger():
+    stream = logging.StreamHandler(sys.stdout)
+    stream.setLevel(logging.DEBUG)
+    log = logging.getLogger('pyzabbix')
+    log.addHandler(stream)
+    log.setLevel(logging.DEBUG)
+
 
 def factory_for(**kwargs):
     databaseinfra = kwargs['databaseinfra']
@@ -27,3 +33,10 @@ def factory_for(**kwargs):
     dbaas_api = DatabaseAsAServiceApi(databaseinfra, credentials)
 
     return ProviderFactory.factory(dbaas_api, zabbix_api=zabbix_api, **kwargs)
+
+
+ZABBIX_INTEGRATION_LOG = os.getenv('ZABBIX_INTEGRATION_LOG') == "1"
+LOG.info('ZABBIX_INTEGRATION_LOG = ' + str(ZABBIX_INTEGRATION_LOG))
+if ZABBIX_INTEGRATION_LOG:
+    LOG.info("Activating log stream for pyzabbix")
+    set_integration_logger()
